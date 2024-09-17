d69f0dd59685802cd3c25611c2565e647c0bb80f
Head In The cloud <hdinthkld@noreply.github.com>
2024-09-17 19:58:26 +0000

    just testing

---
+++ strict_yaml_validator.py
@@ -0,0 +1,20 @@
+from yaml import safe_load
+from pprint import pprint
+from jinja2 import Template
+
+with open("data/site1.yaml", "r") as yaml_file:
+    parsed = safe_load(yaml_file)
+
+with open("templates/sphinx_template.j2", "r") as template_file:
+    template = template_file.read()
+
+#template = """
+#hostname {{site.id}}
+#"""
+
+data = parsed
+
+#pprint(str(parsed["site"]["id"]))
+
+j2_template = Template(template)
+print(j2_template.render(data))
